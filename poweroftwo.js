42c5fa9efa909b7025534c56b3d969b9a406c9c6
Kevin Dai <kevin.dai.2451@gmail.com>
2020-10-02 02:55:16 -0400

    added another js solution

--- poweroftwo.js
+++ poweroftwo.js
@@ -15,3 +15,15 @@ if (n == 0 || n == -2147483648) {
 //Then -4 would be (1011 + 1) or (1100)
 //Finally (0100) & (1100) = (0100) -> only 1 & 1 = 1
 return (n & (-n)) == n
+
+
+//O(1) solution that uses bitwise AND
+
+if (n == 0 || n == -2147483648) {
+    return false
+}
+
+//The idea here is that (n & (n - 1)) turns off the leftmost 1 bit
+//If the value is a power of 2, it only has one 1 bit. If we turn it off,
+//the value should be zero
+return (n & (n - 1)) == 0
